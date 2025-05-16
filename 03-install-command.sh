a42b76b4d61a4492dc841f8d2999d882ffb6607b
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-16 17:29:14 +0530

    messeage written

--- 03-install-command.sh
+++ 03-install-command.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#id= userid, groupid will display
+#sudo id= user id. group id of root will dispaly
+#id -u= only user id will display
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Proceed to install"
+    echo "Don't proceed to install and install with root access"
 else
-    echo "Show the error and please install with roort access"
+    echo "please install with roort access"
 fi
