b32f6fb62556ffab9d57edf8f37979344601c965
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-16 07:12:40 +0530

    messeage written

--- 02-array.sh
+++ 02-array.sh
@@ -2,9 +2,9 @@
 
 boys=("hari" "suri" "mahi" "ahi")
 
-echo "print {boys[0]}"
-echo "print {boys[0]}"
-echo "print {boys[0]}"
-echo "print {boys[0]}"
-echo "print {boys[0]}"
-echo "print {@}"
+echo "print ${boys[0]}"
+echo "print ${boys[1]}
+echo "print ${boys[2]}
+echo "print ${boys[3]}
+echo "print ${boys[4]}
+echo "print {[@]}
