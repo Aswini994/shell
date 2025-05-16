48dcd81008d73dc64d43ffa60699b256fefa03cd
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-16 16:53:16 +0530

    messeage written

--- 01-specvar.sh
+++ 01-specvar.sh
@@ -8,4 +8,5 @@ echo "Home directory of the script, who is running the script $HOME"
 echo "Username of the present running script $USER"
 echo "PID of the script $$"
 echo "PID of last command running in background $!"
+sleep 10 &
 echo "exit status of the last command executed $?"
