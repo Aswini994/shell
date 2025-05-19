d2b0ac8bdd696d348fab0078eb45938f97932da2
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-19 17:37:11 +0530

    colors given

--- 10-loops.sh
+++ 10-loops.sh
@@ -15,10 +15,10 @@ echo "Script started executing at: $(date)"| tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"| tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" &>> $LOG_FILE
     exit 1 #give other than 0 upto 127
 else
-    echo "You are running with root access"| tee -a $LOG_FILE
+    echo "You are running with root access" &>> $LOG_FILE
 fi
 
 # validate functions takes input as exit status, what command they tried to install
