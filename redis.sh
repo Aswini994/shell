c7c21687e95eea506204cbe673a47188a1f8a64c
Aswini994 <aswini.mutyala94@gmail.com>
2025-08-05 16:43:48 +0530

    shell

--- redis.sh
+++ redis.sh
@@ -32,24 +32,24 @@ VALIDATE(){
         exit 1
     fi
 }
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Redis version"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Default Redis version"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis version"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 
-systemctl enable redis
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling redis"
 
-systemctl start redis
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
 
-END_TIME=$(date +%s)
+END_TIME=$(date +%s) &>>$LOG_FILE
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script executed successfully"
