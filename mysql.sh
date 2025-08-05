ad30b6160296885204f96025e99e50f2da79fac7
Aswini994 <aswini.mutyala94@gmail.com>
2025-08-05 17:35:20 +0530

    shell

---
+++ mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)"| tee -a $LOG_FILE
+
+# check the user has root previalages or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N"| tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N"| tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "iNSTALLING MYSQL server" 
+
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld  &>>$LOG_FILE
+
+VALIDATE $? "Starting MYSQL server" 
+
+mysql_secure_installation --set-root-pass RoboShop &>>$LOG_FILE
+VALIDATE $? "Setting MySQL root password"
