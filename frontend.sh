c460127d582c3a6199af955ccaed971065ba62a7
Aswini994 <aswini.mutyala94@gmail.com>
2025-08-05 16:40:41 +0530

    shell

---
+++ frontend.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)"| tee -a $LOG_FILE
+
+# check the user has root previalages or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N"| tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N"| tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module list nginx
+VALIDATE $? "list of nginx modules"
+
+dnf module disable nginx -y
+VALIDATE $? "disabling nginx"
+
+dnf module enable nginx:1.24 -y
+VALIDATE $? "enabling nginx"
+
+dnf install nginx -y
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx 
+systemctl start nginx
+VALIDATE $? "STARTING NGINX"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "DELETING OLD DATA"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "COPYING FRONTEND ZIP"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+VALIDATE $? "UNZIPPING FRONTEND ZIP"
+
+systemctl restart nginx
+VALIDATE $? "Restarting nginx"
+
