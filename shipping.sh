5a6ed67461b81453ff6d1d98c207860f3b89ca1b
Aswini994 <aswini.mutyala94@gmail.com>
2025-08-05 17:46:30 +0530

    shell

---
+++ shipping.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)"| tee -a $LOG_FILE
+
+# check the user has root previalages or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N"| tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N"| tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install maven -y&>>$LOG_FILE
+VALIDATE $? "Installing shipping"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop&>>$LOG_FILE
+VALIDATE $? "creating system user"
+
+mkdir /app
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+cd /app 
+unzip /tmp/shipping.zip&>>$LOG_FILE
+VALIDATE $? "unzippingshipping"
+
+mvn clean package&>>$LOG_FILE
+VALIDATE $? "packaging the shipping application"
+
+mv target/shipping-1.0.jar shipping.jar&>>$LOG_FILE
+VALIDATE $? "moving and renaming jar files"
+
+
+cp  $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
+VALIDATE $? "COPYING shipping SERVICE"
+
+
+systemctl daemon-reload&>>$LOG_FILE
+VALIDATE $? "daemon reload"
+
+systemctl enable shipping &>>$LOG_FILE
+systemctl start shipping
+
+VALIDATE $? "starting shipping"
+
+dnf install mysql -y&>>$LOG_FILE
+VALIDATE $? "installing mysql"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql&>>$LOG_FILE
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql&>>$LOG_FILE
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql&>>$LOG_FILE
+VALIDATE $? "Loading data into mysql"
+
+systemctl restart shipping&>>$LOG_FILE
+VALIDATE $? "Restarting shipping"
+
